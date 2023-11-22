3a6a3c761d47ece60c0f38d94c24f3ec7d60b60f
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-11-22 19:40:23 -0300

    Criando tabela artiss-gender

---
+++ database/migrations/2023_11_22_222426_create_artists_gender_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   private $table = 'artists_gender';
+    public function up(): void
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('artist_id');
+            $table->foreign('artist_id')->references('id')->on('artists');
+            $table->unsignedBigInteger('gender_id');
+            $table->foreign('gender_id')->references('id')->on('genders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->table);
+    }
+};
